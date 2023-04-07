4eede71ba5697fab55e12a8c356e56b7ab4226bf
yastman <homeinfa@gmail.com>
2023-04-07 15:46:09 +0200

    add files

--- js/main.js
+++ js/main.js
@@ -1,23 +1,24 @@
-// let elem1 = document;
-// let elem2 = document.getElementsByClassName("first-view");
-// let elem3 = document.getElementById("section");
-// let elem4 = document.getElementsByTagName("section");
-// let elem5 = document.getElementsByClassName("button-catalogue");
-// let elem6 = document.querySelectorAll("section");
-// let elem7 = document.querySelectorAll("#section");
-// let elem8 = document.querySelector("#section > .layout > article > h2");
-// console.log(elem8);
+let btns = document.querySelectorAll(".button-show-more");
+btns.forEach(function (item) {
+  const popup = document.querySelector(".popup");
+  const closeModal = popup.querySelector(".close_btn");
+  const content = popup.querySelector(".popup_box");
 
-let btn = document.querySelectorAll("#button");
-console.log(btn);
-btn.forEach(function (item) {
+  content.onclick = function (event) {
+    event.stopPropagation();
+  };
   item.onclick = function (event) {
     event.preventDefault();
-    document.querySelector(".popup").classList.add("open");
+    popup.classList.add("open");
   };
+
+  function closePopup(event) {
+    event.preventDefault();
+    popup.classList.remove("open");
+  }
+
+  closeModal.onclick = closePopup;
+  popup.onclick = closePopup;
 });
 
-let closeModal = document.querySelector(".close_btn");
-closeModal.onclick = function () {
-  document.querySelector(".popup.open").classList.remove("open");
-};
+//click outside
