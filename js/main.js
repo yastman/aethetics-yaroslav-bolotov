f6c9fbc6995a86d299191be773dea780a7e30536
yastman <homeinfa@gmail.com>
2023-04-07 17:00:43 +0200

    add homework open modal on button click close modal on button click close modal outside click

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 let btns = document.querySelectorAll(".button-show-more");
 let popup = document.querySelector(".popup");
+
 // click on button to open modal
 btns.forEach(function (item) {
   item.onclick = function (event) {
@@ -8,6 +9,7 @@ btns.forEach(function (item) {
   };
 });
 
+// find  button to close modal
 let closeModal = document.querySelector(".close_btn");
 
 closeModal.onclick = function () {
@@ -16,6 +18,8 @@ closeModal.onclick = function () {
 popup.onclick = function () {
   popup.classList.remove("open");
 };
+
+// outside modal close
 let popupBox = document.querySelector(".popup_box");
 popupBox.onclick = function (event) {
   event.stopPropagation();
