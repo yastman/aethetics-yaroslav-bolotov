4d7d384e80ac5bc794b2568a5777dcc5268a7d1b
yastman <homeinfa@gmail.com>
2023-04-07 16:37:22 +0200

    add homework open modal on button click close moda on button click

--- js/main.js
+++ js/main.js
@@ -1,24 +1,16 @@
 let btns = document.querySelectorAll(".button-show-more");
+let popup = document.querySelector(".popup");
+// click on button to open modal
 btns.forEach(function (item) {
-  const popup = document.querySelector(".popup");
-  const closeModal = popup.querySelector(".close_btn");
-  const content = popup.querySelector(".popup_box");
-
-  content.onclick = function (event) {
-    event.stopPropagation();
-  };
   item.onclick = function (event) {
     event.preventDefault();
     popup.classList.add("open");
   };
-
-  function closePopup(event) {
-    event.preventDefault();
-    popup.classList.remove("open");
-  }
-
-  closeModal.onclick = closePopup;
-  popup.onclick = closePopup;
 });
 
-//click outside
+let closeModal = document.querySelector(".close_btn");
+
+closeModal.onclick = function () {
+  popup.classList.remove("open");
+};
+
