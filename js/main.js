8dcdf3c0b6a6e7958e454882d440081d61c35be7
yastman <homeinfa@gmail.com>
2023-04-10 15:27:00 +0200

    form fix

--- js/main.js
+++ js/main.js
@@ -1,35 +1,42 @@
-let btns = document.querySelectorAll(".button-show-more");
-let popup = document.querySelector(".popup");
+// // let btns = document.querySelectorAll(".button-show-more");
+// // let popup = document.querySelector(".popup");
+// //
+// // // click on button to open modal
+// // btns.forEach(function (item) {
+// //   item.onclick = function (event) {
+// //     event.preventDefault();
+// //     popup.classList.add("open");
+// //   };
+// // });
+// //
+// // // find  button to close modal
+// //
+// // let closeBtn = document.querySelector(".close_btn");
+// //
+// // // add event  to close modal button
+// //
+// // closeBtn.onclick = function () {
+// //   popup.classList.remove("open");
+// // };
+// //
+// // // outside click event
+// //
+// // popup.onclick = function () {
+// //   popup.classList.remove("open");
+// // };
+// //
+// // // outside modal close
+// //
+// // let popupBox = document.querySelector(".popup_box");
+// // popupBox.onclick = function (event) {
+// //   event.stopPropagation();
+// // };
+// //
+// // //homework
+//
 
-// click on button to open modal
-btns.forEach(function (item) {
-  item.onclick = function (event) {
-    event.preventDefault();
-    popup.classList.add("open");
-  };
-});
-
-// find  button to close modal
-
-let closeBtn = document.querySelector(".close_btn");
-
-// add event  to close modal button
-
-closeBtn.onclick = function () {
-  popup.classList.remove("open");
+let form = document.querySelector(".call_me");
+form.onsubmit = function (event) {
+  event.preventDefault();
+  console.log(form.element);
 };
-
-// outside click event
-
-popup.onclick = function () {
-  popup.classList.remove("open");
-};
-
-// outside modal close
-
-let popupBox = document.querySelector(".popup_box");
-popupBox.onclick = function (event) {
-  event.stopPropagation();
-};
-
-//homework
