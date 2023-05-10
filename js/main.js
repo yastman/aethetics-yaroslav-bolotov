77fcd172d747a4c0adea22a4fd795c3e30abde85
yastman <homeinfa@gmail.com>
2023-05-10 20:43:46 +0200

    home work tabs done 1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,19 @@
-let tabs
-let texts
+let tabs = document.querySelectorAll('.tab_item')
+let texts = document.querySelectorAll('.tab_text')
+
+tabs.forEach((tab, index) => {
+	tab.addEventListener('click', () => {
+		tabs.forEach(tab => {
+			tab.classList.remove('active')
+		})
+		tab.classList.add('active')
+
+		texts.forEach(text => {
+			text.style.display = 'none'
+		})
+		texts[index].style.display = 'block'
+	})
+})
+
+texts[0].style.display = 'block'
+tabs[0].classList.add('active')
